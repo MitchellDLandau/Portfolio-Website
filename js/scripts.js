4c4fffdfbd39910a2267dc37488c889f4b4d7383
MitchellDLandau <132216961+MitchellDLandau@users.noreply.github.com>
2023-06-06 10:37:29 -0400

    Form Validation
    I added form validation using javaScript to be able to have real time recognition of requirements when filling out contact information.

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+(function() {
+    let form = document.querySelector('#register-form');
+    let emailInput = document.querySelector('#contact-email');
+    let phoneInput = document.querySelector('#contact-tel');
+  
+  function showErrorMessage (input, message) {
+    let container = input.parentElement;
+  
+    let error = container.querySelector('.error-message');
+    if (error) {
+      container.removeChild(error);
+    } 
+    if (message) {
+      let error = document.createElement ('div');
+      error.classList.add('error-message');
+      error.innerText = message;
+      container.appendChild(error);
+    }
+  }
+    
+    function validateEmail() {
+      let value = emailInput.value;
+      if (!value) {
+        showErrorMessage(emailInput, 'Email is a required field.');
+        return false;
+      }
+      if (value.indexOf('@') === -1) {
+        showErrorMessage(emailInput, 'Email requires an @ sign');
+        return false;
+      }
+      if (value.indexOf('.') === -1) {
+        showErrorMessage(emailInput, 'What is your emails directory');
+        return false;
+      }
+      showErrorMessage(emailInput, null);
+      return true;
+    }
+  
+    function validatePhone () {
+        let phone = phoneInput.value
+
+        if (!phone) {
+            showErrorMessage(phoneInput, 'Phone number is required.');
+            return false;
+        }
+        
+        if (phone.length < 10) {
+            showErrorMessage(phoneInput, 'Area code and phone number required.');
+            return false;
+        }
+
+        showErrorMessage(phoneInput, null);
+        return true;
+        }
+    
+    function validateForm() {
+      let isValidEmail = validateEmail();
+      let isValidPhone = validatePhone();
+      return isValidEmail && isValidPhone;
+    }
+  
+    emailInput.addEventListener ('input', validateEmail);
+    phoneInput.addEventListener ('input', validatePhone);
+    
+    form.addEventListener('submit', (e) => {
+      e.preventDefault(); 
+      if (validateForm()) {
+        alert('Success!');
+      }
+    })
+  
+    // THE RETURN STATEMENT HERE
+  })();
